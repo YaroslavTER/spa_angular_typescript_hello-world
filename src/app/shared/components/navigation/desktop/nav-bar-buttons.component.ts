e55beff747492a958293258200ce04f8a8c8e4bc
Yaroslav Tereshchuk <yaroslavtereshchuk@gmail.com>
2024-03-14 21:32:22 +0200

    Add conditional button show.

--- src/app/shared/components/navigation/desktop/nav-bar-buttons.component.ts
+++ src/app/shared/components/navigation/desktop/nav-bar-buttons.component.ts
@@ -1,7 +1,11 @@
 import { Component } from '@angular/core';
+import { AuthService } from '@auth0/auth0-angular';
 
 @Component({
   selector: 'app-nav-bar-buttons',
   templateUrl: './nav-bar-buttons.component.html',
 })
-export class NavBarButtonsComponent {}
+export class NavBarButtonsComponent {
+  isAuthenticated$ = this.authService.isAuthenticated$;
+  constructor(private authService: AuthService) {}
+}
