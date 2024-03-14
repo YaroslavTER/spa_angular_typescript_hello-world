a4b7752be7431a6f3118c4c87f55518e8061b8dc
Yaroslav Tereshchuk <yaroslavtereshchuk@gmail.com>
2024-03-14 20:55:25 +0200

    Add Auth module using for root.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { environment as env } from '../environments/environment';
+import { AuthModule } from '@auth0/auth0-angular';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    AuthModule.forRoot({
+      ...env.auth0,
+    }),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
