f839eb30454fcfc422e6009420353fc75e98da81
Yaroslav Tereshchuk <yaroslavtereshchuk@gmail.com>
2024-03-18 20:51:02 +0200

    Hide navigation buttons from not authorized users.

--- src/app/shared/components/navigation/desktop/nav-bar-tabs.component.ts
+++ src/app/shared/components/navigation/desktop/nav-bar-tabs.component.ts
@@ -1,7 +1,12 @@
 import { Component } from '@angular/core';
+import { AuthService } from '@auth0/auth0-angular';
 
 @Component({
   selector: 'app-nav-bar-tabs',
   templateUrl: './nav-bar-tabs.component.html',
 })
-export class NavBarTabsComponent {}
+export class NavBarTabsComponent {
+  isAuthenticated$ = this.authService.isAuthenticated$;
+
+  constructor(private authService: AuthService) {}
+}
